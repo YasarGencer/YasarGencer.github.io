d516d4b99ec116e5cdc5618b8bef9142221b31c0
Yaşar Gençer <gncrysr@gmail.com>
2022-10-20 14:13:42 +0300

    Calculator complete

--- Calc/js/script.js
+++ Calc/js/script.js
@@ -6,34 +6,34 @@ const screen = document.querySelector('.screen');
 
 function buttonClick(value){
     if(isNaN(value)){
-         handleSymbol(value);
-    }
-    else{
-        handleNumber(valeu);    
+        handleSymbol(value);
+    }else{
+        handleNumber(value);
     }
     screen.innerText = buffer;
 }
 
 function handleSymbol(symbol){
     switch(symbol){
-        case 'C': 
-            buffer = 0;
+        case 'C':
+            buffer = '0';
             runningTotal = 0;
             break;
         case '=':
-            if(previousOperator == null)
-                return;
-            else
-                flushOperation(parseInt(buffer));
-                previousOperator = null;
-                buffer = runningTotal;
-                runningTotal = 0;
-                break;
+            if(previousOperator === null){
+                return
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = runningTotal;
+            runningTotal = 0;
+            break;
         case '←':
-            if(buffer.length == 1)
-                buffer = 0;
-            else
-                buffer = buffer.substring(0, budffer.length -1);
+            if(buffer.length ===1){
+                buffer = '0';
+            }else{
+                buffer = buffer.substring(0, buffer.length - 1);
+            }
             break;
         case '+':
         case '−':
@@ -45,31 +45,35 @@ function handleSymbol(symbol){
 }
 
 function handleMath(symbol){
-    if(buffer == 0)
+    if(buffer === '0'){
         return;
+    }
+
     const intBuffer = parseInt(buffer);
-    if(runningTotal == 0)
+
+    if(runningTotal === 0){
         runningTotal = intBuffer;
-    else
+    }else{
         flushOperation(intBuffer);
-    
+    }
     previousOperator = symbol;
     buffer = '0';
 }
 
 function flushOperation(intBuffer){
-    if(previousOperator == '+')
+    if(previousOperator === '+'){
         runningTotal += intBuffer;
-    else if(previousOperator == '−')
-    runningTotal -= intBuffer;
-    else if(previousOperator == '÷')
-    runningTotal /= intBuffer;
-    else if(previousOperator == '×')
-    runningTotal *= intBuffer;
+    }else if(previousOperator === '−'){
+        runningTotal -= intBuffer;
+    }else if(previousOperator === '×'){
+        runningTotal *= intBuffer
+    }else if(previousOperator === '÷'){
+        runningTotal /= intBuffer;
+    }
 }
 
 function handleNumber(numberString){
-    if(buffer == "0"){
+    if(buffer === "0"){
         buffer = numberString;
     }else{
         buffer += numberString;
@@ -77,9 +81,9 @@ function handleNumber(numberString){
 }
 
 function init(){
-    document.querySelector('.calc-button').addEventListener('click', function(event){
+    document.querySelector('.calc-buttons').addEventListener('click', function(event){
         buttonClick(event.target.innerText);
-    })  
+    })
 }
 
 init();
